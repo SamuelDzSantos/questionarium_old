5d79ff3e85dd630f1187ef3c4aef6240673a8882
Samuel <samueldzsantos@gmail.com>
2024-05-12 15:28:57 -0300

    Inicio Projeto

---
+++ backend/src/main/java/org/ufpr/questionarium/QuestionariumApplication.java
@@ -0,0 +1,13 @@
+package org.ufpr.questionarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionariumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/ufpr/questionarium/QuestionariumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ufpr.questionarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
