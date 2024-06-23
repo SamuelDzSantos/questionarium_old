f6ac52b24e25edf0d9399894e16e181e7e5ddaac
Samuel <samueldzsantos@gmail.com>
2024-06-23 20:47:35 -0300

    Ajuste das urls permitidas no authguard

--- frontend/src/app/guards/default-can-activate.guard.ts
+++ frontend/src/app/guards/default-can-activate.guard.ts
@@ -5,14 +5,17 @@ import { UserService } from '../services/user.service';
 
 export const defaultCanActivateGuard: CanActivateFn = (route, state) => {
     
+    let allowedUrls=["login","cadastro","","/","sobre","manual","blog","novidades"];
+
     let router = inject(Router)
     let userService = inject(UserService)
     
+
     return userService.getCurrentUser()
     .pipe(
         catchError(()=>{return of(null)}),
         map((user)=>{
-            if(route.url.toString() == "login" || route.url.toString() == "cadastro" || route.url.toString() == "")
+            if(allowedUrls.indexOf(route.url.toString()) != -1)
                 return user == null ? true : router.createUrlTree(["home"])
             else
                 return user == null ? router.createUrlTree(["login"]) : true;
