e14a2573ab2733cf5af20b26bc264990474a6242
Samuel <samueldzsantos@gmail.com>
2024-11-03 17:47:07 -0300

    Adição Auth

---
+++ backend/auth/src/main/java/br/net/dac/auth/interfaces/LoginFormDTO.java
@@ -0,0 +1,5 @@
+package br.net.dac.auth.interfaces;
+
+public record LoginFormDTO(String login, String password) {
+
+}
---
+++ backend/auth/src/main/java/br/net/dac/auth/model/AuthUser.java
@@ -0,0 +1,39 @@
+package br.net.dac.auth.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import br.net.dac.types.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthUser implements UserDetails {
+
+    @Id
+    private String login;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getAsString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+}
