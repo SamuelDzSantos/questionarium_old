baebc6958fdbbdb398f632c3c8c7e72d79ff2e53
yukiqb <yuki.alexandre@ufpr.br>
2024-11-19 13:42:50 -0300

    Criando User service

---
+++ backend/gateway/src/main/java/br/com/questionarium/gateway/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package br.com.questionarium.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("auth", r -> r.path("/auth/**")
+                .uri("http://localhost:14001"))
+
+            .route("user-service", r -> r.path("/user/**")
+                .uri("http://localhost:14002"))
+
+            .route("question-service", r -> r.path("/question/**")
+                .uri("http://localhost:14004"))
+            
+            .route("evaluation-service", r -> r.path("/evaluation/**")
+                .uri("http://localhost:14005"))
+            
+            .route("report-service", r -> r.path("/report/**")
+                .uri("http://localhost:14006"))
+
+            .build();
+    }
+}
---
+++ backend/gateway/src/main/java/br/com/questionarium/gateway/model/User.java
@@ -0,0 +1,19 @@
+package br.com.questionarium.gateway.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
