2e2fe76b85cb69f43e03b8142bc7afef4b8fb0cd
yukiqb <yuki.alexandre@ufpr.br>
2024-12-04 11:52:01 -0300

    atualizacoes

--- frontend/src/app/enviroments/enviroment.ts
+++ frontend/src/app/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 
 export const env = {
     baseUrl: "http://localhost:8080",
-    openAiApiKey: 'sk-LbrCTJFzJyCRE2-_g1B9ryZefxPWkHPI8t2fExEEizT3BlbkFJ5xHXBezfsOC2WvlvTkOGlMKjIWvOSJ_Q1r9-WCWfIA'
+    // openAiApiKey: 'sk-LbrCTJFzJyCRE2-_g1B9ryZefxPWkHPI8t2fExEEizT3BlbkFJ5xHXBezfsOC2WvlvTkOGlMKjIWvOSJ_Q1r9-WCWfIA'
 }
