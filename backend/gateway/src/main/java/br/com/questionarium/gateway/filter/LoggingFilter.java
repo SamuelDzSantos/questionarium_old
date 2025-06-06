c293348bf31a55991039fed1e12733426cd25aa2
yukiqb <yuki.alexandre@ufpr.br>
2025-06-06 01:55:31 -0300

    adicionado tempo limite do circuit breaker de 10s no properties. ajustado dependencia no pom

--- backend/gateway/src/main/java/br/com/questionarium/gateway/filter/LoggingFilter.java
+++ backend/gateway/src/main/java/br/com/questionarium/gateway/filter/LoggingFilter.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Component // Torna este filtro um bean global (aplicado em todas as requisições)
+@Component
 public class LoggingFilter implements GlobalFilter, Ordered {
 
     @Override
