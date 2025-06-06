fac03f8b6fef87279408f924d3091dbf86f8ab83
yukiqb <yuki.alexandre@ufpr.br>
2025-06-06 02:03:37 -0300

    adicionado ao pom dependencia spring starter test e reactor test para testes com webflux e scg

--- backend/curl/gateway/user-post.bat
+++ backend/curl/gateway/user-post.bat
@@ -1,5 +1,5 @@
 @echo off
 curl -X POST "http://localhost:14000/users" ^
   -H "Content-Type: application/json" ^
-  -d "{\"name\":\"ALEXANDRE\",\"email\":\"yuki.alexandre@hotmail.com\",\"password\":\"senha123\",\"roles\":[\"USER\"]}"
+  -d "{\"name\":\"ALE\",\"email\":\"yuki.alexandre@hotmail.com\",\"password\":\"senha123\",\"roles\":[\"USER\"]}"
 pause
