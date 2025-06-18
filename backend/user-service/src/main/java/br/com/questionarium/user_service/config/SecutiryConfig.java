6af578f8cbccefd0ebe7f20520908dbc52f338c7
yukiqb <yuki.alexandre@ufpr.br>
2025-06-18 10:25:47 -0300

    user - application add secret do jwt

--- backend/user-service/src/main/java/br/com/questionarium/user_service/config/SecutiryConfig.java
+++ backend/user-service/src/main/java/br/com/questionarium/user_service/config/SecutiryConfig.java
@@ -1,8 +1,11 @@
 package br.com.questionarium.user_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecutiryConfig {
     
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret}")
+    private String jwtSecret;
 }
